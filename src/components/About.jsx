ab874fea397645144441ef9e962094c5d5b1a11b
WingedGuard <katiepgray@gmail.com>
2020-10-07 11:59:30 -0400

    fixed image paths

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -16,7 +16,7 @@ export default function About(props) {
           <div><img src="https://mediaconsultantshawaii.com/wp-content/uploads/2011/07/mediaconsultantshi-logoclever-e1340737791293.png" alt="media consultants" /></div>
           <div><a href="https://www.hawaiinurses.org/"><img src="http://www.hawaiinurses.org/Portals/local50/HNALOGOsm.png?ver=2017-03-23-222309-440" alt="hawaii nurses" /></a></div>
           <div><a href="https://www.ywcaoahu.org/"><img src="https://static1.squarespace.com/static/532cccc5e4b0e21c66540888/t/56a66ba4a976af914ffb38d9/1576716250191/?format=1500w" alt="ywca" /></a></div>
-          <div><img src="https://github.com/PurpleTatsu/Normil-Media-2020/blob/master/src/images/blue-export-group.jpg?raw=true" alt="blue export group" /></div>
+          <div><img src="https://raw.githubusercontent.com/PurpleTatsu/Normil-Media-2020/main/src/images/blue-export-group.jpg" alt="blue export group" /></div>
           <div><a href="http://firstpaloalto.com/"><img src="https://github.com/PurpleTatsu/Normil-Media-2020/blob/master/src/images/first-palo-alto.png?raw=true" alt="first palo alto" /></a></div>
         </div>
       </div>
